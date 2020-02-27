a681e5471c225f32bbfb466bcd7c80043973dc36
as00979 <as00979@srcs.org>
2020-02-27 14:57:22 -0800

    4.2 completed

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 9;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        float spawnPosY = Random.Range(0, 9);
+        Instantiate(enemyPrefab, new Vector3(spawnPosX, spawnPosY, spawnPosZ), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
